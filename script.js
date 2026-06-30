77a54170bd4d6ef4522fd0384658650996bc2738
Sklenik <99402413+Sklenik@users.noreply.github.com>
2026-06-30 19:51:12 +0200

    feat: disable anti-aliasing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas");
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
 
 const imgSize = 200;
 let playerImg;
